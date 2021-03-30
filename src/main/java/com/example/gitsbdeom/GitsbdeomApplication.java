b5f92a84a1dbea5a2389579a62dcf999d6663be9
songxinlu <337946745@qq.com>
2021-03-30 15:37:55 +0800

    git上创建工程仓库

---
+++ src/main/java/com/example/gitsbdeom/GitsbdeomApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitsbdeom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsbdeomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitsbdeomApplication.class, args);
+    }
+
+}
