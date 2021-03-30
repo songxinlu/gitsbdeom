188462f9f5e449d7fe71481cafc94b687b9a05be
songxinlu <337946745@qq.com>
2021-03-30 16:23:28 +0800

    添加登录功能

--- src/main/java/com/example/gitsbdeom/web/LoginController.java
+++ src/main/java/com/example/gitsbdeom/web/LoginController.java
@@ -1,22 +1,22 @@
 package com.example.gitsbdeom.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @ClassName: HelloController
- * @Description: 欢迎页面控制类
+ * @ClassName: LoginController
+ * @Description: 登录操作控制类
  * @Author songxl
  * @Date 2021/3/30
  * @Version 1.0
  */
 @RestController
-@RequestMapping("hello")
-public class HelloController {
-    @GetMapping("/say")
+@RequestMapping("login")
+public class LoginController {
+    @PostMapping("/login")
     public String sayhello(){
-        return "helloword_again";
+        return "登录成功";
     }
 
 }
